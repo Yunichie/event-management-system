1aaf4b0ee895865048bd608a6d8ba78a88b8f8c4
Yunichie <alfarelsandriano@gmail.com>
2026-05-12 21:44:29 +0700

    feat: implement domain: refund repository

--- crates/domain/src/refund/repository.rs
+++ crates/domain/src/refund/repository.rs
@@ -1,1 +1,11 @@
-// RefundRepository trait
+use async_trait::async_trait;
+
+use crate::shared::errors::RepoError;
+
+use super::{aggregate::Refund, value_objects::RefundId};
+
+#[async_trait]
+pub trait RefundRepository: Send + Sync {
+    async fn save(&self, refund: &mut Refund) -> Result<(), RepoError>;
+    async fn find_by_id(&self, id: RefundId) -> Result<Option<Refund>, RepoError>;
+}
