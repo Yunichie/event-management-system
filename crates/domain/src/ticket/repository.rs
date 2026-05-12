ab42693d698bf54be64ded768b123763a5d41fd6
Yunichie <alfarelsandriano@gmail.com>
2026-05-12 22:17:18 +0700

    feat: implement domain: ticket repository

--- crates/domain/src/ticket/repository.rs
+++ crates/domain/src/ticket/repository.rs
@@ -1,1 +1,16 @@
-// TicketRepository trait
+use async_trait::async_trait;
+
+use crate::shared::errors::RepoError;
+
+use super::{
+    entity::Ticket,
+    value_objects::{TicketCode, TicketId},
+};
+
+#[async_trait]
+pub trait TicketRepository: Send + Sync {
+    async fn save(&self, ticket: &mut Ticket) -> Result<(), RepoError>;
+    async fn save_multiple(&self, tickets: &mut [Ticket]) -> Result<(), RepoError>;
+    async fn find_by_id(&self, id: TicketId) -> Result<Option<Ticket>, RepoError>;
+    async fn find_by_code(&self, code: &TicketCode) -> Result<Option<Ticket>, RepoError>;
+}
