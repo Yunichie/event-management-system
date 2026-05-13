43e9e8b2fdfa9f3ea37d86f4cae1679d6c9b8e39
Yunichie <alfarelsandriano@gmail.com>
2026-05-13 13:09:33 +0700

    fix: implement missing implementations on domain: event repository

--- crates/domain/src/event/repository.rs
+++ crates/domain/src/event/repository.rs
@@ -4,8 +4,15 @@ use crate::shared::errors::RepoError;
 
 use super::{aggregate::Event, value_objects::EventId};
 
+#[derive(Debug, Default)]
+pub struct EventFilter {
+    pub date: Option<chrono::NaiveDate>,
+    pub location: Option<String>,
+}
+
 #[async_trait]
 pub trait EventRepository: Send + Sync {
     async fn save(&self, event: &mut Event) -> Result<(), RepoError>;
     async fn find_by_id(&self, id: EventId) -> Result<Option<Event>, RepoError>;
+    async fn find_published(&self, filter: EventFilter) -> Result<Vec<Event>, RepoError>;
 }
